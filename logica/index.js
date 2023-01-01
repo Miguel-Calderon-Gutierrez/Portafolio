fe21dcaacd0132778a2be5b62812e6e097658d56
Miguel Calderon <miguel280641@gmail.com>
2023-01-01 18:27:30 -0500

    formulario terminado

--- logica/index.js
+++ logica/index.js
@@ -1,17 +1,21 @@
-const enviarMensaje = () =>{
-    console.log();
+const enviarMensaje = () => {
+  console.log();
 
-    let mail = document.createElement("a");
-    let asunto = document.getElementById("asunto").value;
-    let Mensaje = document.getElementById("cuerpo").value;
-    
-    asunto = asunto.replace(/ /gi, "%20");
-    Mensaje = Mensaje.replace(/ /gi, "%20");
+  let mail = document.createElement("a");
+  let asunto = document.getElementById("asunto").value;
+  let Mensaje = document.getElementById("cuerpo").value;
 
-    referencia = "mailto:miguelcalderon.dev@gmail.com?subject="+asunto+"&body="+Mensaje;
-    mail.href = referencia;
-    mail.click();
-}
+  asunto = asunto.replace(/ /gi, "%20");
+  Mensaje = Mensaje.replace(/ /gi, "%20");
+
+  referencia =
+    "mailto:miguelcalderon.dev@gmail.com?subject=" +
+    asunto +
+    "&body=" +
+    Mensaje;
+  mail.href = referencia;
+  mail.click();
+};
 
 let button = document.getElementById("btnenviar");
-button.addEventListener("click",enviarMensaje);
+button.addEventListener("click", enviarMensaje);
