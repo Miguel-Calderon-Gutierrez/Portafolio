43e7dd8ec245c60bbb17e990c8a9756b547a3477
Miguel Calderon <miguel280641@gmail.com>
2023-01-01 18:25:22 -0500

    Creando formulario formulario terminado

--- logica/index.js
+++ logica/index.js
@@ -2,20 +2,16 @@ const enviarMensaje = () =>{
     console.log();
 
     let mail = document.createElement("a");
-    let asunto = "hola mundo soy Miguel"
-    let Mensaje = "Este es el mensaje a enviar"
+    let asunto = document.getElementById("asunto").value;
+    let Mensaje = document.getElementById("cuerpo").value;
     
     asunto = asunto.replace(/ /gi, "%20");
     Mensaje = Mensaje.replace(/ /gi, "%20");
 
     referencia = "mailto:miguelcalderon.dev@gmail.com?subject="+asunto+"&body="+Mensaje;
-
     mail.href = referencia;
     mail.click();
 }
-
-
-alert("Hola mundo")
 
 let button = document.getElementById("btnenviar");
 button.addEventListener("click",enviarMensaje);
